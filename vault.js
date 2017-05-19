87e09c0b7f85551b8ec15a14ad36ef0a0855cc74
Alexis <alexisko@hawaii.edu>
2017-05-18 19:19:03 -1000

    All tests passing

--- vault.js
+++ vault.js
@@ -1,4 +1,21 @@
 'use strict';
 module.exports = function() {
-  
+	var _key;
+	var _value;
+  	function setValue(key, value) {
+  		_key = key;
+  		_value = value;
+  	}
+
+  	function getValue(key) {
+  		if(_key) {
+  			return _value;
+  		}
+  		return null;
+  	}
+
+  	return {
+  		setValue: setValue,
+  		getValue: getValue
+  	};
 };
